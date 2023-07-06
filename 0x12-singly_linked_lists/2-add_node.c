51fff5c0804e4d9d5cdfb3bc273dba3806adaba9
bobjoan <bobjoan966@gmail.com>
2023-07-06 05:40:46 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <string.h>
 /**
  * add_node - adds a new node at the beginning of a list_t list.
  * @head: linked list
@@ -19,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	newNode->str = str;
+	newNode->str = strdup(str);
 	newNode->next = *head;
 	*head = newNode;
 
