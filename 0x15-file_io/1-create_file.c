f9b6c15cf01a9d4be063490e660f7c0bf69108bb
bobjoan <bobjoan966@gmail.com>
2023-07-11 09:53:46 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	ssize_t bytesWritten;
 	int file;
 
 	if (filename == NULL)
@@ -19,17 +20,22 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		write(file, text_content, strlen(text_content));
-	}
+		bytesWritten = write(file, text_content, strlen(text_content));
 
+		if (bytesWritten == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
 	close(file);
 	return (1);
 }
